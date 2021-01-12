241b83a29b2f739026c4488d34d3b6189c8b1410
brynknowles <bryn.knowles@gmail.com>
2021-01-11 21:08:38 -0500

    lab done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function focusHandler(event) {
+        return console.log("Good!", event)
+    }
+
+    function blurHandler(event) {
+        return console.log("Hey! Eyes on me!", event)
+    }
+
+    return (
+        <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    function handleChange(event) {
+        // return console.log(`${event.target.name}: ${event.target.value}`)
+        return console.log("Entering password...", event.target.value)
+
+    }
+
+    return (
+        <div>
+            <input 
+                type="password" 
+                name="search"
+                onChange={handleChange}
+                placeholder="please enter password..."
+            />
+        </div>
+    )
+
+}
+
+export default Keypad;
